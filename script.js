820cf529ae4214d1e0b7941a6aa76d49b1dc726f
irvanln <irvina22@gmail.com>
2020-09-24 09:30:53 +0700

    javascript function

--- script.js
+++ script.js
@@ -5,4 +5,26 @@
         document.write('<h1>' +
         "The value of z is: " + z +'</h1>');
         console.log(z);
-    
+
+ console.log(        typeof "-123.30");
+
+ var pulpen;
+pulpen =["red","blue","green","orange"];
+
+var pulpenMerah = pulpen[2];
+
+console.log(pulpenMerah);
+
+var result;
+//contoh fungsi
+function myFunction(p1, p2) {
+    var hasil;
+    hasil = p1*p2;
+    return hasil;
+  }
+
+  console.log(myFunction(4, 3));
+
+  //tampilkan di browser
+  result = myFunction(4, 3);
+  document.write(result);
