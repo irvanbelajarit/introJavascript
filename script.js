49582654f09bb1846d724c5c7aa3611ee0de5e2f
irvanln <irvina22@gmail.com>
2020-09-24 08:40:13 +0700

    intro insert javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+       
+        var x = 5;
+        var y = 15;
+        var z = x + y;
+        document.write('<h1>' +
+        "The value of z is: " + z +'</h1>');
+        console.log(z);
+    
